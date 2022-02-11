6114cba9431ba4677e7aebf6b2244cb2c00e94da
AngusWG <740713651@qq.com>
2022-02-11 15:08:19 +0800

    feat: Support specified templates and support git record filtering.

---
+++ track_changes/__main__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# encoding: utf-8 
+# @Time    : 2022/2/11 10:36
+# @author  : zza
+# @Email   : 740713651@qq.com
+# @File    : __main__.py
+from track_changes import entrypoint
+
+if __name__ == '__main__':
+    entrypoint()
